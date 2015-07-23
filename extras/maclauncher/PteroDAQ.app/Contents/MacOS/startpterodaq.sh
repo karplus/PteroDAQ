3a7ff0580d8fe478534d01c4b152bf31b2f2471f
Kevin Karplus <karplus@soe.ucsc.edu>
2015-07-22 21:52:47 -0700

    modified startpterodaq.sh to point to the real source, if nothing gets moved (which is still very, very fragile).

--- extras/maclauncher/PteroDAQ.app/Contents/MacOS/startpterodaq.sh
+++ extras/maclauncher/PteroDAQ.app/Contents/MacOS/startpterodaq.sh
@@ -5,4 +5,4 @@ if [ -z $PYBIN ]; then PYBIN=`type -P python`; fi
 if [ -z $PYBIN ]; then exit; fi
 rm -f $(dirname "$0")/guipython
 ln -s `$PYBIN -c 'import sys; print(sys.prefix)'`/Resources/Python.app/Contents/MacOS/Python $(dirname "$0")/guipython
-$(dirname "$0")/guipython $(dirname "$0")/../Resources/daq
+$(dirname "$0")/guipython $(dirname "$0")/../../../../../daq
