984f3fb4122b361438dd48e87ede90026b87feb6
Kevin Karplus <karplus@soe.ucsc.edu>
2015-08-03 09:48:42 -0700

    Merged  extras/changefirmware_Linux.sh and extras/frdm-firmware-update.sh into extras/frdm-firmware-update-linux.sh

--- extras/frdm-firmware-update-linux.sh
+++ extras/frdm-firmware-update-linux.sh
@@ -1,18 +1,19 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
 # From http://karibe.co.ke/2014/04/changing-the-firmware-on-freescale-freedom-boards-in-linux/
 # for Linux boxes
 #	First argument is where the BOOTLOADER is mounted
 #	Second argument is where the new firmware is on your filesystem
 #	Third argument is a new name to give as a mount point temporarily
-DEVICE=$1 #1st command line argument, e.g /dev/sdb
-FIRMWARE="$2" #2nd command line argument, e.g /path/to/CMSIS-DAP_OpenSDA.S19
-MNTPOINT="$3" #3rd command line argument, e.g /mnt/myfolder created with 'sudo mkdir /mnt/myfolder 
+DEVICE="$1" #1st command line argument, e.g., /dev/sdc
+FIRMWARE="$2" #2nd command line argument, e.g., ~/Downloads/20140530_k20dx128_kl25z_if_opensda.s19
+MNTPOINT="$3" #3rd command line argument, e.g., /mnt/myfolder created with 'sudo mkdir /mnt/myfolder 
 sudo mkdir -p $MNTPOINT #create a mount point, dont complain if it exists
 if [ ! -e $DEVICE ]; then echo "Can't find device"; exit 1; fi # check if device exists
-sudo umount  $DEVICE #unmount automounted device
+sudo umount  "$DEVICE" #unmount automounted device
 sudo modprobe msdos # wake msdos module
-sudo mount -t msdos $DEVICE $MNTPOINT # mount device with msdos type
+sudo mount -t msdos "$DEVICE" "$MNTPOINT" # mount device with msdos type
 sudo cp "$FIRMWARE" "$MNTPOINT" # copy the firmware
 sync # write any data buffered in memory out to disk
-sleep 1 # we have to wait (experiment :))
-sudo umount $DEVICE # i need not explain :)
+sleep 2 # delay for flash erase/program (delay may need to be adjusted)
+sudo umount "$DEVICE" # unmount the device
+
