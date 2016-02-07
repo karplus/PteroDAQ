10b1a89068b0d9a4ef908836237b727942f1ed54
Keving Karplus <karplus@soe.ucsc.edu>
2016-02-06 17:19:39 -0800

    user: Kevin Karplus <karplus@soe.ucsc.edu> changed daq/getports/macgetports.py    Fixed method for finding ports to work with Arduinos as well as    Teensy boards.

--- daq/getports/macgetports.py
+++ daq/getports/macgetports.py
@@ -63,9 +63,9 @@ def getname(dev):
 def portiter():
     """generator that yields pairs (name, /dev string)  for USB serial ports
     """
-    # look for an IOSerialBSDClient which has an ancestor 
-    #     that is an IOUSBHostInterface or IOUSBInterface
-    # and get the name from the parent of the of that
+    # look for an IOSerialBSDClient which has an ancestor
+    #     that is an AppleUSBEHCI or AppleUSB20HubPort
+    # and get the name from the child of that
     itr = ctypes.c_void_p() # iterator of interfaces
     # populate the iterator with all SerialBSDClients
     iokit.IOServiceGetMatchingServices(kIOMasterPortDefault, iokit.IOServiceMatching(b'IOSerialBSDClient'), ctypes.byref(itr))
@@ -73,29 +73,131 @@ def portiter():
         service = iokit.IOIteratorNext(itr) # going through each item in the iterator
         if not service: # a null pointer means we've exhausted the iterator
             break
-        # print("DEBUG: starting from ", getname(service), file=sys.stderr)
-        interface = service
+#        print("DEBUG: starting from ", getname(service), file=sys.stderr)
+        child = service
         while True:
-            # print("DEBUG: looking for parent of ", getname(interface), file=sys.stderr)
+#            print("DEBUG: looking for parent of ", getname(child), file=sys.stderr)
             parent = ctypes.c_void_p()
             response = iokit.IORegistryEntryGetParentEntry(
-                interface,
+                child,
                 b"IOService",
                 ctypes.byref(parent))
             if response != 0:
-                # Unable to find a parent for the interface, we're done.
+                # Unable to find a parent for the child, we're done.
                 break
-            if getname(interface) in [b'IOUSBInterface', b'IOUSBHostInterface']:
-                # found  interface and parent
+            if getname(parent) in [b'AppleUSBEHCI', b'AppleUSB20HubPort']:
+                # found  child and parent
                 break
-            interface = parent
+            child = parent
         if response!=0:
             continue
-        
-        # print("DEBUG: found interface", getname(service), "parent", getname(parent), file=sys.stderr)	
-        # print("DEBUG: getstring(service, kIODialinDeviceKey)=", getstring(service, kIODialinDeviceKey), file=sys.stderr)	
 
-        yield (getname(parent), getstring(service, kIODialinDeviceKey)) # device name and dialin address         
+#        print("DEBUG: found child", getname(child), "parent", getname(parent), file=sys.stderr)	
+#        print("DEBUG: getstring(service, kIODialinDeviceKey)=", getstring(service, kIODialinDeviceKey), file=sys.stderr)	
+
+        yield (getname(child), getstring(service, kIODialinDeviceKey)) # device name and dialin address
 #        iokit.IOObjectRelease(service)
 #        iokit.IOObjectRelease(interface)
-    iokit.IOObjectRelease(itr)
+#    iokit.IOObjectRelease(itr)
+
+# MAC OS X 10.6.8
+# Parental chain for Teensy LC board:
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IOModemSerialStreamSync
+# DEBUG: looking for parent of  AppleUSBCDCACMData
+# DEBUG: looking for parent of  IOUSBInterface
+# DEBUG: found interface IOSerialBSDClient parent PteroDAQ (Teensy)
+
+# continues with
+# DEBUG: looking for parent of  PteroDAQ (Teensy)
+# DEBUG: looking for parent of  AppleUSBEHCI
+# DEBUG: looking for parent of  EHC2
+# DEBUG: looking for parent of  AppleACPIPCI
+# DEBUG: looking for parent of  PCI0
+# DEBUG: looking for parent of  AppleACPIPlatformExpert
+# DEBUG: looking for parent of  MacBookPro5,3
+# DEBUG: looking for parent of  Root
+
+# Almost identical chain for KL25Z board:
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IOModemSerialStreamSync
+# DEBUG: looking for parent of  AppleUSBCDCACMData
+# DEBUG: looking for parent of  IOUSBInterface
+# DEBUG: found interface IOSerialBSDClient parent PteroDAQ (FRDM-KL25Z)
+
+# Parental chain for Arduino Duemilanove and Sparkfun Redboard:
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IORS232SerialStreamSync
+# DEBUG: looking for parent of  FTDIUSBSerialDriver
+# DEBUG: looking for parent of  FT232R USB UART
+# DEBUG: looking for parent of  FT232R USB UART
+# DEBUG: looking for parent of  AppleUSBEHCI
+# DEBUG: looking for parent of  EHC2
+# DEBUG: looking for parent of  AppleACPIPCI
+# DEBUG: looking for parent of  PCI0
+# DEBUG: looking for parent of  AppleACPIPlatformExpert
+# DEBUG: looking for parent of  MacBookPro5,3
+# DEBUG: looking for parent of  Root
+
+# Note that above the IOSerialBSDClient the first common name between
+# Kinetis-processor boards and the Arduino boards is AppleUSBEHCI,
+# which is the parent of the device name we want to use.
+
+# undesired Bluetooth devices:
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IOBluetoothSerialClientSerialStreamSync
+# DEBUG: looking for parent of  IOBluetoothSerialClient
+# DEBUG: looking for parent of  IOBluetoothSerialManager
+# DEBUG: looking for parent of  IOResources
+# DEBUG: looking for parent of  MacBookPro5,3
+# DEBUG: looking for parent of  Root
+
+################################################
+# Mac OS X 10.11.3 has different USB hierarchy:
+################################################
+
+# undesired Bluetooth looks the same
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IOBluetoothSerialClientSerialStreamSync
+# DEBUG: looking for parent of  IOBluetoothSerialClient
+# DEBUG: looking for parent of  IOBluetoothSerialManager
+# DEBUG: looking for parent of  IOResources
+# DEBUG: looking for parent of  iMac12,2
+# DEBUG: looking for parent of  Root
+
+# Arduino FTDI interfaces look the same
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IORS232SerialStreamSync
+# DEBUG: looking for parent of  FTDIUSBSerialDriver
+# DEBUG: looking for parent of  FT232R USB UART
+# DEBUG: looking for parent of  FT232R USB UART
+# DEBUG: found child FT232R USB UART parent AppleUSBEHCI
+
+# Teensy board has a very different stack!
+# DEBUG: starting from  IOSerialBSDClient
+# DEBUG: looking for parent of  IOSerialBSDClient
+# DEBUG: looking for parent of  IOModemSerialStreamSync
+# DEBUG: looking for parent of  AppleUSBACMData
+# DEBUG: looking for parent of  IOUSBHostInterface
+# DEBUG: looking for parent of  PteroDAQ (Teensy)
+# DEBUG: looking for parent of  AppleUSB20HubPort
+# DEBUG: looking for parent of  AppleUSB20Hub
+# DEBUG: looking for parent of  USB 2.0 Hub
+# DEBUG: looking for parent of  AppleUSB20HubPort
+# DEBUG: looking for parent of  AppleUSB20InternalHub
+# DEBUG: looking for parent of  IOUSBHostDevice
+# DEBUG: looking for parent of  PRT1
+# DEBUG: looking for parent of  EHC2
+# DEBUG: looking for parent of  EHC2
+# DEBUG: looking for parent of  AppleACPIPCI
+# DEBUG: looking for parent of  PCI0
+# DEBUG: looking for parent of  AppleACPIPlatformExpert
+# DEBUG: looking for parent of  iMac12,2
+# DEBUG: looking for parent of  Root
+
